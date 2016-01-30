acbe06e1178b3794e7b4306a8203453ea00178eb
Andras Kucsma <andras.kucsma@prezi.com>
2016-01-30 17:23:44 +0100

    Add readMatrix

--- stuff.h
+++ stuff.h
@@ -21,6 +21,17 @@
 #include <cassert>
 
 template <typename T>
+std::vector<std::vector<T>> readMatrix(int height, int width) {
+	std::vector<std::vector<T>> matrix(height, std::vector<T>(width));
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			std::cin >> matrix[y][x];
+		}
+	}
+	return matrix;
+}
+
+template <typename T>
 std::ostream& operator<<(std::ostream& out, const std::vector<T>& v) {
 	out << '{' << v.size() << '}' << '[';
 	std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ","));
